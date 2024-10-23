9e614154c6f9fc15b1820671a688b13e979a4acf
Eshaan Deshmukh <eshaan@Laptop-Probably.local>
2024-10-23 10:41:08 -0500

    stufffff

--- timer.gd
+++ timer.gd
@@ -2,13 +2,8 @@ extends Node2D
 
 var time = 0
 
-func _ready():
-	pass
-
 func _process(delta):
 	$header.text = 'Timer: ' + str(time)
-	if (time == 0):
-		$beep.play()
 
 
 func _on_start_pressed():
@@ -17,4 +12,4 @@ func _on_start_pressed():
 	for i in range(time):
 		await get_tree().create_timer(1).timeout
 		time -= 1
-	await get_tree().create_timer(1).timeout
+	$beep.play()
